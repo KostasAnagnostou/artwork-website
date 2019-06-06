4adcc813302a18cbad7c9ed6e393ab119f8a8f32
Kostas Anagnostou <anagnostou.kostas@gmail.com>
2019-06-06 20:10:16 +0200

    remove-emails

--- form.php
+++ form.php
@@ -1,19 +1,13 @@
 <!-- Example Here => https://stackoverflow.com/questions/5335273/how-to-send-an-email-using-php       -->
 <?php 
 
-    $to = "artist.ms.contact@gmail.com"; // this is your Email address
-    $from = "forward@konstantinos-anagnostou.com";
+    $to = ""; // this is your Email address
+    $from = "";
     $email = test_input($_POST['email']); // this is the sender's Email address
     $name = test_input($_POST['name']);
     $subject = "New Email Contact via the Web Form";
     $message = "The user with name: " . $name. "\n\n" . "and email: " . $email . "\n\n" . " wrote the following:" . "\n\n" . test_input($_POST['message']);
     
-    // Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-
     $headers .= "From:" . $from;
     
     mail($to,$subject,$message,$headers);
